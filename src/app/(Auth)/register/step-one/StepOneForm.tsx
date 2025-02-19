24ea88cb1c02e6cdeae333f0a0636dff32904d42
Hassan Mahadjir <hm.mahadjir@gmail.com>
2025-02-19 22:57:45 +0200

    play with register

--- src/app/(Auth)/register/step-one/StepOneForm.tsx
+++ src/app/(Auth)/register/step-one/StepOneForm.tsx
@@ -8,6 +8,7 @@ import FormInput from "@/components/FormInput";
 import { Form } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
 
 const formSchema = z.object({
   email: z.string().email({ message: "Invalid email address." }),
@@ -30,7 +31,7 @@ export default function StepOneForm() {
 
   return (
     <div className="flex flex-1 flex-col items-center bg-white mt-14 w-3/4 h-[500px] justify-self-center rounded-3xl">
-      <div className="mt-24 text-center">
+      <div className="mt-16 text-center mb-8">
         <h1 className=" text-2xl font-semibold mb-1">Create an account</h1>
         <p className="text-xs">
           Already have an ccount?{" "}
@@ -39,8 +40,52 @@ export default function StepOneForm() {
           </a>
         </p>
       </div>
+      {/* Navigation */}
+      <div className="">
+        <div className="flex justify-center items-center mb-3">
+          {/* First step */}
+          <div className="flex flex-col items-center justify-center">
+            <div className=" w-5 h-5 rounded-full text-white bg-dark-gray text-center">
+              <span>1</span>
+            </div>
+          </div>
+          <div className="flex-grow border-t border-gray-400 w-48 mx-3"></div>
+          {/* second step  */}
+          <div className="flex flex-col items-center justify-center">
+            <div className=" w-5 h-5 rounded-full text-white bg-dark-gray text-center">
+              <span>2</span>
+            </div>
+          </div>
+          <div className="flex-grow border-t border-gray-400 w-48 mx-3"></div>
+          {/* Third step  */}
+          <div className="flex flex-col items-center justify-center">
+            <div className=" w-5 h-5 rounded-full text-white bg-dark-gray text-center">
+              <span>3</span>
+            </div>
+          </div>
+        </div>
+      </div>
 
-      <div className="flex w-full flex-col gap-8 w-2/3">
+      <div className="max-w-3xl max-auto">
+        <div className="flex justify-center items-center mb-3">
+          {/* First step */}
+          <div className="flex flex-col items-center justify-center">
+            <p>Enter your email</p>
+          </div>
+          <div className="flex-grow w-24 mx-3"></div>
+          {/* second step  */}
+          <div className="flex flex-col items-center justify-center">
+            <p>Provide basic info</p>
+          </div>
+          <div className="flex-grow w-24 mx-3"></div>
+          {/* Third step  */}
+          <div className="flex flex-col items-center justify-center">
+            <p>Create password</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex-col gap-4 w-4/6">
         <Form {...form}>
           <form>
             <FormInput
@@ -67,6 +112,33 @@ export default function StepOneForm() {
             {t("continueWith")}
           </span>
           <div className="flex-grow border-t border-gray-400"></div>
+        </div>
+
+        {/* other accounts */}
+        <div className="flex flex-col items-center justify-center gap-4 w-full md:flex-row">
+          <button className="flex items-center justify-center gap-x-2 w-full xs:w-1/2 px-8 py-3 border-2 border-[var(--main)] rounded-3xl hover:text-white hover:bg-second-main transition-colors duration-150">
+            <Image
+              src="/auth/microsoft.png"
+              width={24}
+              height={24}
+              alt="Microsoft logo"
+              className="h-5 w-5"
+            />
+            <span className="text-sm font-medium">
+              {t("loginWithMicrosoft")}
+            </span>
+          </button>
+
+          <button className="flex items-center justify-center gap-x-2 w-full xs:w-1/2 px-8 py-3 border-2 border-[var(--main)] rounded-3xl hover:text-white hover:bg-second-main transition-colors duration-150">
+            <Image
+              src="/auth/GoogleGlogo.svg.webp"
+              width={24}
+              height={24}
+              alt="Google logo"
+              className="h-5 w-5"
+            />
+            <span className="text-sm font-medium">{t("loginWithGoogle")}</span>
+          </button>
         </div>
       </div>
     </div>
