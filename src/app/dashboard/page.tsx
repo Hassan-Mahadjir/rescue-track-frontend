a8665fd676c64ec1385f507a70eab62a26cf2248
Hassan Mahadjir <hm.mahadjir@gmail.com>
2025-02-25 01:12:27 +0200

    header of dashboard almost done

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,59 +1,79 @@
-import clsx from "clsx";
 import Link from "next/link";
 import React from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { GiMedicinePills } from "react-icons/gi";
+import { MdMarkEmailUnread } from "react-icons/md";
+import { PiAmbulanceFill } from "react-icons/pi";
+import { IoDocumentText } from "react-icons/io5";
 
 const Dashboard = () => {
+  const items = [
+    {
+      header: "Drugs about to expire",
+      description:
+        "Monitor medication inventory and oreorder supplies as needed.",
+      icon: <GiMedicinePills className="w-16 h-16" color="white" />,
+      navTitle: "Open Inventory",
+      link: "/inventory",
+    },
+    {
+      header: "New received Messages",
+      description: "You’ve received new messages check them out.",
+      icon: <MdMarkEmailUnread className="w-16 h-16" color="white" />,
+      navTitle: "Open Messages",
+      link: "/messages",
+    },
+    {
+      header: "Available EMS dispatchers",
+      description:
+        "Monitor EMS in real-time for instant visibility and control.",
+      icon: <PiAmbulanceFill className="w-16 h-16" color="white" />,
+      navTitle: "Open EMS Tracker",
+      link: "/ems",
+    },
+    {
+      header: "Shift Swapping requests",
+      description: "You have received new shift swapping requests.",
+      icon: <IoDocumentText className="w-16 h-16" color="white" />,
+      navTitle: "Open requests",
+      link: "/shift",
+    },
+  ];
+
   return (
     <div className="mx-3">
-      <div className="flex flex-row items-center gap-2">
-        <div className="bg-gradient-to-t from-main to-second-green rounded-2xl py-3 px-2 min-w-fit max-w-xs">
-          {/* Amount number with Message */}
-          <div className="flex items-center gap-2 min-w-0">
-            <div className="bg-light-gray rounded-3xl p-1 flex-shrink-0 hidden xmd:block">
-              <span className="font-bold">299</span>
-            </div>
-            <p className="text-sm font-semibold text-white whitespace-nowrap xmd:text-xl px-3 hidden xmd:block">
-              Drugs about to expire
-            </p>
-          </div>
-
-          <div className="flex mt-1 items-center flex-wrap min-w-0">
-            {/* description */}
-            <div className="flex flex-col items-center hidden xmd:block min-w-0">
-              <p className="text-sm text-white w-3/4 hidden xmd:block ">
-                Monitor medication inventory and reorder supplies as needed.
-              </p>
-              <Link href={"#"} className="hover:bg-main">
-                <div className="flex py-1 px-2 mt-2 rounded-xl bg-brown hover:text-white hover:bg-second-main transition-colors duration-500 items-center space-x-2 text-main w-fit">
-                  <span className="px-3 text-md font-semibold whitespace-nowrap">
-                    Open Inventory
+      <div className="flex flex-row items-center gap-2 overflow-x-auto max-w-full [&::-webkit-scrollbar]:hidden [&::-webkit-scrollbar-thumb]:hidden"></div>
+      <div className="flex items-center gap-2">
+        {items.map((item) => (
+          <div
+            key={item.link}
+            className="w-1/4 p-2 bg-gradient-to-t from-main to-second-green rounded-2xl overflow-hidden"
+          >
+            <div className="flex items-center justify-center">
+              <div className="hidden xmd:block">
+                <div className="flex items-center gap-2">
+                  <span className="p-1 font-bold rounded-full bg-light-gray text-main">
+                    999
                   </span>
-                  <FaArrowRightLong className="w-6 h-5" />
+                  <p className="text-sm font-semibold text-white">
+                    {item.header}
+                  </p>
                 </div>
-              </Link>
+                <p className="text-sm text-white mt-1">{item.description}</p>
+              </div>
+              <div>{item.icon}</div>
             </div>
 
-            {/* icon */}
-
-            <div className="flex flex-col items-center">
-              <GiMedicinePills
-                className="w-20 h-20 -ml-0 xmd:-ml-32"
-                color="white"
-              />
-              {/* open wanted page  */}
-              <Link href={"#"}>
-                <div className="flex items-center justify-center rounded-xl bg-brown text-main px-3 py-1 mt-2 hover:text-white hover:bg-second-main transition-colors duration-500 xmd:hidden w-fit ">
-                  <span className="text-md font-semibold whitespace-nowrap">
-                    Open Inventory
-                  </span>
-                  <FaArrowRightLong className="w-6 h-5 ml-2" />
-                </div>
-              </Link>
-            </div>
+            <Link href={item.link} className="hover:bg-main">
+              <div className="flex items-center py-1 px-2 mt-2 rounded-xl bg-brown hover:text-white hover:bg-second-main transition-colors duration-500 space-x-2 text-main overflow-hidden whitespace-nowrap w-full justify-center xmd:justify-between xmd:w-2/3 ">
+                <span className="text-sm font-semibold min-w-0 max-w-full hidden truncate sm:block">
+                  {item.navTitle}
+                </span>
+                <FaArrowRightLong className="w-5 h-5 shrink-0 " />
+              </div>
+            </Link>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
