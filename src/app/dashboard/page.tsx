941c451dbccc63c05b7fb86509e04431f22e1ecc
Hassan Mahadjir <hm.mahadjir@gmail.com>
2025-02-25 22:17:14 +0200

    List of Doctors

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,49 +1,12 @@
-import Link from "next/link";
 import React from "react";
-import { FaArrowRightLong } from "react-icons/fa6";
-import { GiMedicinePills } from "react-icons/gi";
-import { MdMarkEmailUnread } from "react-icons/md";
-import { PiAmbulanceFill } from "react-icons/pi";
-import { IoDocumentText } from "react-icons/io5";
-import clsx from "clsx";
 import RecentCard from "@/components/dashboard/Recent-Card";
+import InformationGrid from "@/components/dashboard/Information-Grid";
 
 const Dashboard = () => {
-  const items = [
-    {
-      header: "Drugs about to expire",
-      description: "Monitor medication inventory and oreorder supplies.",
-      icon: <GiMedicinePills className="w-16 h-16" color="white" />,
-      navTitle: "Open Inventory",
-      link: "/inventory",
-    },
-    {
-      header: "New received Messages",
-      description: "You’ve received new messages check them out.",
-      icon: <MdMarkEmailUnread className="w-16 h-16" color="white" />,
-      navTitle: "Open Messages",
-      link: "/messages",
-    },
-    {
-      header: "Available EMS dispatchers",
-      description:
-        "Monitor EMS in real-time for instant visibility and control.",
-      icon: <PiAmbulanceFill className="w-16 h-16" color="white" />,
-      navTitle: "Open EMS Tracker",
-      link: "/ems",
-    },
-    {
-      header: "Shift Swapping requests",
-      description: "You have received new shift swapping requests.",
-      icon: <IoDocumentText className="w-16 h-16" color="white" />,
-      navTitle: "Open requests",
-      link: "/shift",
-    },
-  ];
-
   return (
-    <div className="mx-3">
+    <div className="mx-3 space-y-3">
       <RecentCard />
+      <InformationGrid />
     </div>
   );
 };
