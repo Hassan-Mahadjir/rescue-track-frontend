e94e2cf2953a346867ed4da64ee7ba64b36868bc
Hassan Mahadjir <hm.mahadjir@gmail.com>
2025-03-06 22:28:25 +0200

    Service card Component

--- src/app/(Report Management)/report/page.tsx
+++ src/app/(Report Management)/report/page.tsx
@@ -1,9 +1,17 @@
 "use client";
 
-import { useState } from "react";
-import ServiceCard from "@/components/Service-Card";
+import Carousel from "@/components/Cards-Carousel";
 
 const slides = [
+  {
+    title: "Medical History",
+    imageSrc: "/report/health-check.png",
+    buttons: [
+      { label: "Create new", onClick: () => console.log("Create new") },
+      { label: "View all", onClick: () => console.log("View all") },
+    ],
+  },
+
   {
     title: "Patient Care Report",
     imageSrc: "/report/patient.png",
@@ -12,17 +20,10 @@ const slides = [
       { label: "View all", onClick: () => console.log("View all") },
     ],
   },
-  {
-    title: "Medical History",
-    imageSrc: "/report/medical.png",
-    buttons: [
-      { label: "Create new", onClick: () => console.log("Create new") },
-      { label: "View all", onClick: () => console.log("View all") },
-    ],
-  },
+
   {
     title: "Lab Reports",
-    imageSrc: "/report/lab.png",
+    imageSrc: "/report/regulatory.png",
     buttons: [
       { label: "Create new", onClick: () => console.log("Create new") },
       { label: "View all", onClick: () => console.log("View all") },
@@ -31,10 +32,8 @@ const slides = [
 ];
 
 export default function SwiperComponent() {
-  const [currentPage, setCurrentPage] = useState(1);
-
   return (
-    <div className="flex flex-col items-center text-center justify-center">
+    <div className="flex flex-col items-center text-center justify-center mt-5">
       <h1 className="text-2xl font-semibold text-main">
         Choose a report category
       </h1>
@@ -43,47 +42,7 @@ export default function SwiperComponent() {
       </p>
 
       {/* Slider */}
-      <div className="relative flex items-center mt-5 gap-5 perspective-1000">
-        {slides.map((slide, index) => {
-          const isActive = index === currentPage;
-
-          return (
-            <div
-              key={slide.title}
-              className="transition-transform duration-500 cursor-pointer"
-              onClick={() => setCurrentPage(index)} // Set clicked slide as current
-              style={{
-                transform: isActive
-                  ? "scale(1) translateZ(50px)" // Active slide pops forward
-                  : "scale(0.9) translateZ(-20px)", // Others appear slightly behind
-                opacity: isActive ? 1 : 0.8, // Dim non-active slides slightly
-              }}
-            >
-              <ServiceCard {...slide} />
-            </div>
-          );
-        })}
-      </div>
-
-      {/* Navigation */}
-      <div className="mt-4 flex gap-4">
-        <button
-          className="px-4 py-2 bg-white text-black rounded"
-          onClick={() =>
-            setCurrentPage((prev) => (prev > 0 ? prev - 1 : slides.length - 1))
-          }
-        >
-          Prev
-        </button>
-        <button
-          className="px-4 py-2 bg-white text-black rounded"
-          onClick={() =>
-            setCurrentPage((prev) => (prev < slides.length - 1 ? prev + 1 : 0))
-          }
-        >
-          Next
-        </button>
-      </div>
+      <Carousel slides={slides} />
     </div>
   );
 }
---
+++ src/components/Cards-Carousel.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import ServiceCard from "./Service-Card";
+
+interface CarouselProps {
+  slides: {
+    title: string;
+    imageSrc?: string;
+    icon?: React.ReactNode;
+    buttons: { label: string; onClick: () => void }[];
+  }[];
+}
+
+const Carousel: React.FC<CarouselProps> = ({ slides }) => {
+  const startIndex = slides.length === 1 ? 0 : 1;
+
+  const [currentPage, setCurrentPage] = useState<number>(startIndex);
+
+  return (
+    <div className="flex items-center mt-5 gap-5 perspective-1000 flex-col xmd:flex-row">
+      {slides.map((slide, index) => {
+        const isActive = index === currentPage;
+
+        return (
+          <div
+            key={slide.title}
+            className="transition-transform duration-500 cursor-pointer"
+            onClick={() => setCurrentPage(index)} // Set clicked slide as current
+            style={{
+              transform: isActive
+                ? "scale(1) translateZ(50px)" // Active slide pops forward
+                : "scale(0.9) translateZ(-20px)", // Others appear slightly behind
+              opacity: isActive ? 1 : 0.9, // Dim non-active slides slightly
+            }}
+          >
+            <ServiceCard {...slide} />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Carousel;
