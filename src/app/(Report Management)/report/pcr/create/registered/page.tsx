2b781457bc06bb4df2c33856b5343221ce8f85b3
Nouralden Elhouni <noralden.honi@gmail.com>
2025-03-25 23:16:25 +0200

    registered patient information page with tabs for medication and crew details

--- src/app/(Report Management)/report/pcr/create/registered/page.tsx
+++ src/app/(Report Management)/report/pcr/create/registered/page.tsx
@@ -1,7 +1,143 @@
+import { PCRFormContextProider } from "@/components/PCRFormContextProvider";
+import PatientPersonalInfo from "@/components/report/PatientPersonalInfo";
+import CrewTab from "@/components/report/pcr/CrewTab";
+import IncidentTab from "@/components/report/pcr/IncidentTab";
+import MedicationTab from "@/components/report/pcr/MedicationTab";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Trash2 } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 const Registered = () => {
-  return <div>Registered</div>;
+  const patientData = {
+    fullName: "Mahamat Hassan Mahadjir Hassan",
+    age: 24,
+    phone: "+90 533 867 28 35",
+    email: "hm.mahadjir@gmail.com",
+    profileImage: "/report/sample.jpg",
+    identifyNumber: "20910394",
+    dateOfBirth: "17-05-2000",
+    nationality: "Chad",
+    address: "Northern Cyprus",
+    sex: "Male",
+    height: 189,
+    weight: 74,
+    bloodType: "- O",
+  };
+
+  return (
+    <div className="mx-5 my-2">
+      <div className="space-y-4">
+        <div>
+          <Input />
+          <Button>Search</Button>
+        </div>
+        <Tabs defaultValue="patient_Info">
+          <TabsList className="mb-4 flex space-x-4 justify-start">
+            <TabsTrigger value="patient_Info">Patient Information</TabsTrigger>
+            <TabsTrigger value="medication_info">
+              Medication information
+            </TabsTrigger>
+            <TabsTrigger value="crew_Info">Crew Information</TabsTrigger>
+            <TabsTrigger value="medical_history">Medical History</TabsTrigger>
+          </TabsList>
+          <TabsContent value="patient_Info">
+            <div className="space-y-4">
+              {/*Patient personal information */}
+              <PatientPersonalInfo patient={patientData} />
+              <PatientPersonalInfo patient={patientData} />
+            </div>
+          </TabsContent>
+          <TabsContent value="medication_info">
+            <div className="space-y-4">
+              <h2 className="text-lg font-medium mb-3">
+                Treatment provided during transport
+              </h2>
+              <Card>
+                <div className="space-y-2">
+                  <div>
+                    <div>
+                      <div>
+                        #
+                        <Select>
+                          <SelectTrigger>
+                            <SelectValue placeholder="Select medication" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="Paracetamol 500mg">
+                              Paracetamol 500mg
+                            </SelectItem>
+                            <SelectItem value="Ibuprofen 400mg">
+                              Ibuprofen 400mg
+                            </SelectItem>
+                            <SelectItem value="Morphine 10mg">
+                              Morphine 10mg
+                            </SelectItem>
+                            <SelectItem value="Aspirin 300mg">
+                              Aspirin 300mg
+                            </SelectItem>
+                          </SelectContent>
+                        </Select>
+                      </div>
+                    </div>
+                    <div>
+                      <div>
+                        #
+                        <Select>
+                          <SelectTrigger>
+                            <SelectValue placeholder="Dosage" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="500 mg">500 mg</SelectItem>
+                            <SelectItem value="1000 mg">1000 mg</SelectItem>
+                            <SelectItem value="250 mg">250 mg</SelectItem>
+                            <SelectItem value="100 mg">100 mg</SelectItem>
+                          </SelectContent>
+                        </Select>
+                      </div>
+                    </div>
+
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      className="h-8 w-8 text-red-500 hover:text-red-600 hover:bg-red-50 rounded-full"
+                    >
+                      <Trash2 className="h-4 w-4" />
+                      <span className="sr-only">Remove medication</span>
+                    </Button>
+                  </div>
+                </div>
+              </Card>
+            </div>
+          </TabsContent>
+          <TabsContent value="crew_Info">
+            <div className="space-y-4">Crew Information</div>
+          </TabsContent>
+          <TabsContent value="medical_history">
+            <div className="space-y-4">Medical History</div>
+          </TabsContent>
+        </Tabs>
+        <div className="flex items-center justify-center space-x-4">
+          <Button variant="outline" size="lg" disabled={true}>
+            prev
+          </Button>
+          <Button size="lg" className="bg-main">
+            next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Registered;
